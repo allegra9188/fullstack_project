292a043de7531115592332cc677fb3f03339da16
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2023-11-07 14:11:24 -0500

    moved tasks.js to /server/api/students and renamed students.js. Cleaned up and adjusted starter code to fit our project.

---
+++ src/server/api/students/students.js
@@ -0,0 +1,69 @@
+const { ServerError } = require("../../errors");
+const prisma = require("../../prisma");
+
+const router = require("express").Router();
+module.exports = router;
+
+/** retrieves all students */
+router.get("/", async (req, res, next) => {
+  try {
+    const students = await prisma.student.findMany({
+      // TODO
+    });
+    res.json(students);
+  } catch (err) {
+    next(err);
+  }
+});
+
+/** Creates new student */
+router.post("/", async (req, res, next) => {
+  try {
+    // TODO
+  } catch (err) {
+    next(err);
+  }
+});
+
+/** retrieves single student by id */
+// DONE? TO-Do - Double Check
+router.get("/:id", async (req, res, next) => {
+  try {
+    const id = +req.params.id;
+    const student = await prisma.task.findUnique({ where: { id } });
+    res.json(student);
+  } catch (err) {
+    next(err);
+  }
+});
+
+/** Updates single student by id */
+router.put("/:id", async (req, res, next) => {
+  try {
+    const id = +req.params.id;
+
+    const student = await prisma.task.findUnique({ where: { id } });
+
+    const updatedStudent = await prisma.task.update({
+      where: { id },
+      // TODO (below)
+      data: {},
+    });
+    res.json(updatedStudent);
+  } catch (err) {
+    next(err);
+  }
+});
+
+/** Deletes single student by id */
+// DONE? TO-Do - Double Check
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const id = +req.params.id;
+    const student = await prisma.task.findUnique({ where: { id } });
+    await prisma.task.delete({ where: { id } });
+    res.sendStatus(204);
+  } catch (err) {
+    next(err);
+  }
+});
