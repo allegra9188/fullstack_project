c30b7f0999dd93dd224871116db8bf52785e4b32
Allegra McDermott <amcdermott9188@gmail.com>
2023-11-08 06:43:41 -0800

    Got rid of the Auth functions

--- src/client/store/api.js
+++ src/client/store/api.js
@@ -8,11 +8,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const api = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "/api",
-    prepareHeaders: (headers, { getState }) => {
-      const token = getState().auth.token;
-      if (token) {
-        headers.set("authorization", `Bearer ${token}`);
-      }
+    prepareHeaders: (headers) => {
       return headers;
     },
   }),
