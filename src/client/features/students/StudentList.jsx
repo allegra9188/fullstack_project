cf80726f2a2e3edae17ce51a609e5418b3a57a00
Allegra McDermott <amcdermott9188@gmail.com>
2023-11-08 12:41:25 -0800

    Added Student function working, with button

--- src/client/features/students/StudentList.jsx
+++ src/client/features/students/StudentList.jsx
@@ -2,9 +2,10 @@ import { useState } from "react";
 import StudentCard from "./StudentCard";
 import StudentDetails from "./StudentDetails";
 import { useGetStudentsQuery } from "./studentSlice";
+import { Link } from "react-router-dom";
 
 import "./Students.less";
-import "./StudentList.scss"
+import "./StudentList.scss";
 
 // reference from Book Buddy
 /** Main interface for user to interact with their tasks */
@@ -18,24 +19,29 @@ export default function Students() {
     <h2>Loading students...</h2>
   ) : (
     <main>
-      <h1>Students</h1>
+      <br />
+      <h2>Students:</h2>
       <form>
-        <input 
-        type="text" 
-        placeholder="Search..."
-        onChange={(e) => setFilter(e.target.value)}
+        <input
+          type="text"
+          placeholder="Search Student..."
+          onChange={(e) => setFilter(e.target.value)}
         />
       </form>
+      <button>
+        <Link to="/students/add">Add Student</Link>
+      </button>
+      <br />
       <ul className="student-list">
         {[...students]
-          .filter((student) => (student.firstName + student.lastName).match(searchRegex))
-          .sort((a,z) => a.firstName.localeCompare(z.firstName))
+          .filter((student) =>
+            (student.firstName + student.lastName).match(searchRegex)
+          )
+          .sort((a, z) => a.firstName.localeCompare(z.firstName))
           .map((student) => (
-            <StudentCard key={student.id} student={student}/>
-          ))
-        }
+            <StudentCard key={student.id} student={student} />
+          ))}
       </ul>
-
     </main>
   );
 }
