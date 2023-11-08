41e16626cbdb2cc368e9393dee3a042df1703e9e
Allegra McDermott <amcdermott9188@gmail.com>
2023-11-08 11:29:18 -0800

    Delete function added to front page

--- src/client/features/students/StudentCard.jsx
+++ src/client/features/students/StudentCard.jsx
@@ -1,19 +1,34 @@
 import { Link } from "react-router-dom";
+import {
+  useDeleteStudentMutation,
+  useEditStudentMutation,
+  useGetStudentQuery,
+} from "./studentSlice";
 
 // A Student Card displays a brief preview of the Student in StudentList
 
 export default function StudentCard({ student }) {
-    
-    return (
-        <li className="student-card">
-            <div className="student-image">
-                <img src={student.imageUrl} alt="" />
-            </div>
-            <section className="student-info">
-                <h2>{student.firstName}</h2>
-                <h2>{student.lastName}</h2>
-            <Link to={`/students/${student.id}`}>More Info</Link>
-            </section>
-        </li>
-    )
+  const [deleteStudent] = useDeleteStudentMutation();
+
+  /** Deletes the task */
+  const onDelete = async (evt) => {
+    evt.preventDefault();
+    deleteStudent(student.id);
+  };
+
+  return (
+    <li className="student-card">
+      <div className="student-image">
+        <img src={student.imageUrl} alt="" />
+      </div>
+      <section className="student-info">
+        <h2>{student.firstName}</h2>
+        <h2>{student.lastName}</h2>
+        <Link to={`/students/${student.id}`}>More Info</Link>
+        <button onClick={onDelete} aria-label="delete">
+          Delete Student
+        </button>
+      </section>
+    </li>
+  );
 }
