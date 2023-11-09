9d3a7853e3e98f6641c0a3492d2e478ebe42d6bf
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2023-11-09 12:33:04 -0500

    refactored sort by to be on all list pages, not as SecondaryNav

--- src/client/features/students/StudentsByGpa.jsx
+++ src/client/features/students/StudentsByGpa.jsx
@@ -16,10 +16,33 @@ export default function Students() {
   const [filter, setFilter] = useState("");
   const searchRegex = new RegExp(filter, "i");
 
+  const handleSort = (e) => {
+    e.preventDefault();
+    console.log(e.target.value);
+    if (e.target.value === "last-name") {
+      navigate("/students/sortedbylastname");
+    }
+    if (e.target.value === "first-name") {
+      navigate("/students");
+    }
+    if (e.target.value === "gpa") {
+      navigate("/students/sortedbygpa");
+    }
+  };
+
   return isLoading ? (
     <h2>Loading students...</h2>
   ) : (
     <main>
+      <div>
+        <label>Sort By</label>
+        <select id="sort-by" onChange={handleSort}>
+          <option value="Please select"></option>
+          <option value="first-name">First Name</option>
+          <option value="last-name">Last Name</option>
+          <option value="gpa">GPA</option>
+        </select>
+      </div>
       <br />
       <form>
         <input
--- src/client/layout/Root.jsx
+++ src/client/layout/Root.jsx
@@ -2,13 +2,11 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 
 import "./Root.less";
-import SecondaryNav from "../features/SecondaryNav";
 
 export default function Root() {
   return (
     <>
       <Navbar />
-      <SecondaryNav />
       <main>
         <Outlet />
       </main>
